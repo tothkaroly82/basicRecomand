0ae733a4014b1cf6475be91ec508341595eddb57
tkaroly82@gmail.com <tkaroly82@gmail.com>
2023-07-06 14:34:38 +0300

    ranking by NrDoc

---
+++ sql_select.sql
@@ -0,0 +1,21 @@
+Declare @Dela as date='2018-01-01'
+Declare @PanaLA as date='2023-06-30'
+Declare @PromCode as varchar(20)='2023LichidareStoc'
+select lp.ITEMID ItemId, s.ClientId, Valoare, NrDoc, Qty
+from (select xaig.itemid 
+			from XMI_AdditionalItemGroups xaig 
+			where  xaig.DATAAREAID='MIG'
+				and xaig.AdditionalItemGroupID=@PromCode
+			) lp
+left join (select s.INVOICEACCOUNT ClientId, s.ItemId, sum(s.LINEAMOUNTMST) Valoare, count(s.InvoiceID) NrDoc, sum(s.QTY) Qty
+			from OLAP_DAXELES.dbo.Sales s
+				join XMI_AdditionalItemGroups xaig on xaig.ITEMID=s.ItemID
+									and xaig.DATAAREAID='MIG'
+									and xaig.AdditionalItemGroupID=@PromCode 
+				join ( select distinct [Cod client]
+			from DaxAdditional.tk.ListaEmailDist ls ) ls on ls.[Cod client]=s.INVOICEACCOUNT
+			where s.INVOICEDATE between @Dela and @PanaLA
+			group by s.INVOICEACCOUNT, s.ItemID ) s on s.ItemID=lp.ITEMID
+
+order by lp.ITEMID
+;
